bf7f5e2fa63072aa79ead9e3ef7b919f97d57a6f
mohit2308jain <38962127+mohit2308jain@users.noreply.github.com>
2019-06-04 18:41:34 +0530

    Files Added

---
+++ Arrays/Basics/Rotation of array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void rrotate(long long arr[], long long n){
+    long long t = arr[n-1],i;
+    for(i=n-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=t;
+}
+void lrotate(long long arr[], long long n){
+    long long t = arr[0],i;
+    for(i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=t;
+}
+int main()
+{
+    long long n,i,k,l;
+    cin>>n>>k;
+    long long arr[n+1];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    l=(n-k);
+    if(l>k){
+        for(i=0;i<k;i++){
+            lrotate(arr,n);
+        }
+    }
+    else{
+        for(i=0;i<l;i++){
+            rrotate(arr,n);
+        }
+    }
+
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
