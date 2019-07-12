b38aed2e0a6ef1255bbc4f8bae225dd55e94e98c
Mohit Jain <mohitjain5000@gmail.com>
2019-07-12 23:15:59 +0530

    File added

---
+++ Trees/Level of a node in bt.cpp
@@ -0,0 +1,29 @@
+/*This is a function problem.You only need to complete the function given below*/
+/*
+Structure of the node of the binary tree is
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return level of the target node
+int gee(Node* root, int target,int level){
+    if(!root) return 0;
+    int a,b;
+    if(root->data==target) return (++level);
+    else{
+        level++;
+        a = gee(root->left,target,level);
+        if(a>=level) return a;
+        
+        b = gee(root->right,target,level);
+        if(b>=level) return b;
+    }
+}
+int getLevel(struct Node *node, int target)
+{
+    int level = 0;
+    gee(node,target,level);
+}
