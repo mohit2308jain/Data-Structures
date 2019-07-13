80f08e5efdb54d408d55390e5926006f820f80d6
mohit2308jain <38962127+mohit2308jain@users.noreply.github.com>
2019-07-13 21:37:01 +0530

    update

--- Strings/X pattern.cpp
+++ Strings/X pattern.cpp
@@ -9,6 +9,41 @@ int main(){
 		for(i=0;i<n;i++){
 		    k = n-1-i;
 			for(j=0;j<n;j++){
+				if(j==i || j==k) cout<<s[i];
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+}
+/**********************
+
+Example - 
+program
+
+p     p
+ r   r 
+  o o  
+   g   
+  r r  
+ a   a 
+m     m
+
+*******************/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	string s;
+	cin>>s;
+	int i,j,k,n = s.length();
+	if(n%2==0) cout<<"not possible";
+	else{
+		for(i=0;i<n;i++){
+		    k = n-1-i;
+			for(j=0;j<n;j++){
 				if(j==i || j==k) cout<<s[j];
 				else cout<<" ";
 			}
@@ -17,3 +52,52 @@ int main(){
 	}
 	return 0;
 }
+
+/**********************
+
+Example - 
+program
+
+p     m
+ r   a 
+  o r  
+   g   
+  o r  
+ r   a 
+p     m
+
+***************/
+
+#include<iostream>
+using namespace std;
+int main(){
+	string s;
+	cin>>s;
+	int i,j,k,n = s.length();
+	if(n%2==0) cout<<"not possible";
+	else{
+		for(i=0;i<n;i++){
+		    k = n-1-i;
+			for(j=0;j<n;j++){
+				if(j==i || j==k) cout<<s[k];
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+}
+/**********************
+
+Example - 
+program
+
+m     m
+ a   a 
+  r r  
+   g   
+  o o  
+ r   r 
+p     p
+
+***************/
